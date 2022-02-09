0603e9f197e025e82f076bffb8da7cc39c77c04d
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2022-02-09 05:31:39 -0400

    Added Triangle.h

---
+++ Triangle.h
@@ -0,0 +1,19 @@
+
+
+class Triangle {
+
+public:
+
+    double side1;
+    double side2;
+    double side3;
+
+    int color;
+
+    Triangle(double s1, double s2, double s3) {
+        side1 = s1;
+        side2 = s2;
+        side3 = s3;
+    }
+
+};
